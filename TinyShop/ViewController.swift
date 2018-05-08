f55c9ccf8d894172c78cc9cb3974a46f529e21b5
Callum Boddy <callum.boddy@gmail.com>
2018-05-08 13:02:49 +0100

    Initial Commit

---
+++ TinyShop/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinyShop
+//
+//  Created by Callum Boddy on 08/05/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
